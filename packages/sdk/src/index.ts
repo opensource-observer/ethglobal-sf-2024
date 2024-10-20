5d6c2664d19d503b057333434dcbb44543c4aaac
Javier R <javier.rios-urbano@epitech.eu>
2024-10-20 02:18:12 +0200

    add: `cli` entrypoint

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello, world!");
+import yargs from "yargs";
+import { updateWeights } from "./metrics/index.js";
+import { populateDb } from "./utils/misc.js";
+
+const args = await yargs(process.argv.slice(2))
+  .command("populate:db", "Populate the database with initial data")
+  .command("update:weights", "Update weights in the Split")
+  .command("dispatch:email", "Dispatch email with the new weights")
+  .demandCommand(1)
+  .help().argv;
+
+const [command] = args._;
+
+const commandMap: {
+  [key: string]: () => unknown;
+} = {
+  "populate:db": async () => {
+    const result = await populateDb();
+    if (result.isFail()) {
+      throw result.unwrapFail();
+    }
+  },
+  "update:weights": async () => {
+    const result = await updateWeights();
+    if (result.isFail()) {
+      throw result.unwrapFail();
+    }
+  },
+  "dispatch:email": () => void 0,
+};
+
+await commandMap[command]();
