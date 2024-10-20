345299071dd90053d5e4a65dab39b6f2e2772c59
Javier Ríos <javier.rios-urbano@epitech.eu>
2024-10-19 20:51:25 -0700

    feat: referral `split` impl (#3)
    * add: enable manual triggering for distribution workflow
    
    * feat: enhance funding allocation logic with `referral` shares
    
    * feat: add `percentage` split referral
    
    * fix: ignore `invalid` addresses
    
    * fix: only run `cron` automatically

--- packages/sdk/src/metrics/computeWeights.ts
+++ packages/sdk/src/metrics/computeWeights.ts
@@ -58,42 +58,94 @@ export const computeWeights = async (): Promise<
       totalCommits += metrics.commitCount;
     });
 
-    const contributorsShares = metrics.map(({ metrics, info }) => {
+    const contributorsShares = metrics.map(async ({ metrics, info }) => {
       const normalizedPRs = metrics.prCount / totalPRs;
       const normalizedIssues = metrics.issueCount / totalIssues;
       const normalizedLinesAdded = metrics.linesAdded / totalLinesAdded;
       const normalizedLinesDeleted = metrics.linesDeleted / totalLinesDeleted;
       const normalizedCommits = metrics.commitCount / totalCommits;
 
-      const normalizedLines =
-        (normalizedLinesAdded + normalizedLinesDeleted) / 2;
+      const normalizedLines = (normalizedLinesAdded + normalizedLinesDeleted) /
+        2;
 
-      const share =
-        prWeight * normalizedPRs +
+      const share = prWeight * normalizedPRs +
         issueWeight * normalizedIssues +
         linesWeight * normalizedLines +
         commitWeight * normalizedCommits;
 
-      return {
+      if (info.referrer === null) {
+        return [{
+          contributor: {
+            id: info.user_id,
+            wallet: info.wallet_address,
+            artifact_namespace: info.artifact_namespace,
+            artifact_name: info.artifact_name,
+          },
+          sharePercentage: share * 100,
+        }];
+      }
+
+      const queryParams = new URLSearchParams(info.referrer);
+
+      const referrerId = queryParams.has("referrer")
+        ? queryParams.get("referrer")!
+        : info.referrer;
+
+      const { data: referrer, error } = await supabase.from(
+        "pool_registrations",
+      )
+        .select("*").eq(
+          "pool_id",
+          pool.id,
+        ).eq("user_id", referrerId);
+
+      const recipients = [];
+
+      if (!error) {
+        core.info(
+          `${referrer[0].artifact_namespace}/${
+            referrer[0].artifact_name
+          } referred ${info.artifact_namespace}/${info.artifact_name}`,
+        );
+
+        recipients.push({
+          contributor: {
+            id: referrer[0].user_id,
+            wallet: referrer[0].wallet_address,
+            artifact_namespace: referrer[0].artifact_namespace,
+            artifact_name: referrer[0].artifact_name,
+          },
+          sharePercentage: share,
+        });
+      }
+
+      recipients.push({
         contributor: {
           id: info.user_id,
           wallet: info.wallet_address,
           artifact_namespace: info.artifact_namespace,
           artifact_name: info.artifact_name,
         },
-        sharePercentage: share * 100,
-      };
+        sharePercentage: error ? share * 100 : share * 99,
+      });
+
+      return recipients;
     });
 
-    const fundingDistribution = contributorsShares.map((contributorShare) => {
-      return {
-        contributor: contributorShare.contributor,
-        allocatedFunding: +(
-          (contributorShare.sharePercentage / 100) *
-          fundingPool
-        ).toFixed(4),
-      };
-    });
+    const recipients = (await Promise.all(contributorsShares)).flat();
+
+    const fundingDistribution = recipients
+      .map(
+        (contributorShare) => {
+          return {
+            contributor: contributorShare.contributor,
+            allocatedFunding: +(
+              (contributorShare.sharePercentage / 100) *
+              fundingPool
+            ).toFixed(4),
+          };
+        },
+      );
 
     fundingWeights[pool.id] = fundingDistribution;
 
